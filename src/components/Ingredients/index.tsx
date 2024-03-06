2410e8d3e2ccb6e16ca0fad14c80d2593e412b23
O-orion <lucas.lvtorres@gmail.com>
2024-03-05 23:19:03 -0300

    animando card alert

--- src/components/Ingredients/index.tsx
+++ src/components/Ingredients/index.tsx
@@ -1,7 +1,8 @@
-import { FlatList, ScrollView } from "react-native";
+import { FlatList, ScrollView, View, Alert } from "react-native";
 import { Ingredient } from "../Ingredient";
 import { style } from "./style";
 import { useState } from "react";
+import { Selected } from "../selected";
 
 export function Ingredients () {
 
@@ -17,19 +18,35 @@ export function Ingredients () {
         console.log(selected)
     }
 
+    function handleClearSelected () {
+        Alert.alert("Limpar", "Deseja limpar tudo ?", [
+            {text: 'Não', style: 'cancel'},
+            {text: 'Sim', onPress: () =>  setSelected([]) },
+        ])
+       
+    }
+
     return (
         // Criando uma lista
-        <ScrollView contentContainerStyle={style.container} showsVerticalScrollIndicator={false}>
-            {
-                Array.from({ length: 100}).map((item, index) => (
-                    <Ingredient  
-                    key={index} name="tomate" 
-                    image="" 
-                    selected={selected.includes(String(index))} 
-                    onPress={() => handleToggleSelected(""+index)}
-                     />
-                ))
-            }
-        </ScrollView>
+        <>
+            <ScrollView contentContainerStyle={style.container} showsVerticalScrollIndicator={false}>
+                {
+                    Array.from({ length: 100}).map((item, index) => (
+                        <Ingredient  
+                        key={index} name="tomate" 
+                        image="" 
+                        selected={selected.includes(String(index))} 
+                        onPress={() => handleToggleSelected(""+index)}
+                         />
+                    ))
+                }
+            </ScrollView>
+            { selected.length > 0 && (
+                <Selected quantity={selected.length} onClear={handleClearSelected} onSearch={()=>{}}></Selected>
+
+            )}
+        
+        </>
+
     )
 }
---
+++ src/components/selected/index.tsx
@@ -0,0 +1,26 @@
+import  Animated, { SlideInDown, BounceOutDown } from "react-native-reanimated";
+import { View, Text } from "react-native";
+import { style } from "./style";
+import {  MaterialIcons } from '@expo/vector-icons'
+import { theme } from "@/theme";
+
+type Props = {
+    quantity: number,
+    onClear: () =>  void,
+    onSearch: () => void
+}
+
+export function Selected({ quantity, onClear, onSearch }: Props) {
+    console.log('oi')
+    console.log(quantity)
+    return (
+        <Animated.View style={style.container} entering={SlideInDown.duration(500)} exiting={BounceOutDown}>
+            <View style={style.header}>
+                <Text style={style.label} >
+                    {quantity} ingredientes selecionados
+                </Text>
+                <MaterialIcons name="close" size={24} onPress={onClear} color={theme.colors.gray_400}></MaterialIcons>
+            </View>
+        </Animated.View>
+    )
+}
